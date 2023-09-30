9b0e5eb1a6e8889ac6692888dc7f5e84a46db79a
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-09-30 22:58:52 +0200

    Update ProductComponent.tsx

--- src/components/ProductComponent.tsx
+++ src/components/ProductComponent.tsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
 import { Product } from "../types/product";
+import { useAtom } from "jotai";
+import { carritoAtom } from "../store/store";
 
 export default function ProductComponent({product}: {product: Product}){
-    return (
+  const [carrito, setCarrito] = useAtom(carritoAtom)
+  const addToCart = () => {
+    setCarrito([...carrito, product])
+  }
+
+  return (
   <div className="max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden">
   <div className="p-4">
     <Link to={`/product/${product.id}`} className="font-bold text-xl mb-2 hover:underline">{product.name}</Link>
     <p className="text-gray-700 text-base">{product.description}</p>
     <div className="mt-4 flex items-center justify-between">
       <span className="font-bold text-xl">{product.price}€</span>
-      <button className={`btn btn-primary m-2`} disabled={product.quantity === 0} title={product.quantity === 0 ? "No hay stock" : "Agregar al carrito"}>
+      <button className={`btn btn-primary m-2`} disabled={product.quantity === 0} title={product.quantity === 0 ? "No hay stock" : "Agregar al carrito"} onClick={addToCart}>
         Agregar al carrito
       </button>
     </div>
