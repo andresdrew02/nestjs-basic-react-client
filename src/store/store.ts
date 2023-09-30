e3904b0e1bb74115fd821ef27cc152c37eddfaba
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-09-30 22:58:59 +0200

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,6 @@
 import { atom } from "jotai";
 import { User } from "../types/user";
+import { Carrito } from "../types/carrito";
 
 export const userAtom = atom<User>(null)
+export const carritoAtom = atom<Carrito>([])
