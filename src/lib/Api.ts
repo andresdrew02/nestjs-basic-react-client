2b0975f077d1dcd42018c34ac4f4b6a5ab907dd5
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-09-30 22:20:26 +0200

    Update Api.ts

--- src/lib/Api.ts
+++ src/lib/Api.ts
@@ -1,5 +1,6 @@
 import { ProductFilters } from "../pages/Tienda";
-import { ProductList } from "../types/product";
+import { Error } from "../types/error";
+import { Product, ProductList } from "../types/product";
 
 export async function getProducts(filters: ProductFilters = {}, query?: string): Promise<ProductList>{
     let queryString = ''
@@ -10,9 +11,19 @@ export async function getProducts(filters: ProductFilters = {}, query?: string):
     }
     if (filters.stock){
         queryString += filterCounter === 0 ? `?stock=${filters.stock}` : `&stock=${filters.stock}`
+        filterCounter++
     }
     if (filters.price){
         queryString += filterCounter === 0 ? `?price=${filters.price}` : `&price=${filters.price}`
+        filterCounter++
+    }
+    if (filters.page){
+        queryString += filterCounter === 0 ? `?page=${filters.page}` : `&page=${filters.page}`
+        filterCounter++
     }
     return (await fetch('http://localhost:3000/api/products' + `${queryString}`)).json()
+}
+
+export async function getProductById(id: number): Promise<Product | Error>{
+    return (await fetch(`http://localhost:3000/api/products/${id}`)).json()
 }
