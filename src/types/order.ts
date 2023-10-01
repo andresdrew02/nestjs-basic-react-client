e5cbde33d546ec39b46558e79d57e41dd0d55a68
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-10-01 19:50:45 +0200

    Create UserOrderItem

--- src/types/order.ts
+++ src/types/order.ts
@@ -6,3 +6,26 @@ export type OrderItem = {
     productId: number,
     quantity: number
 }
+
+export type UserOrderItem = {
+    id: number
+    userId: string
+    orderDate: string
+    status: string
+    orderItems: [{
+        id: number,
+        productId: number,
+        orderId: number,
+        quantity: number,
+        totalLine: number,
+        product: {
+            id: number,
+            name: string
+            price: number
+            quantity: number
+            description: string
+        }
+    }]
+}
+
+export type UserOrders = UserOrderItem[]
