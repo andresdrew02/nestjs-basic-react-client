87bce0257f5dc38ad412d745ecbd72815985a032
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-10-01 18:28:27 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 
 export default function Home(){
     return(
-        <Hero/>
+        <>
+        <div className="min-h-screen">
+            <Hero/>
+        </div>
+        <Footer/>
+        </>
     )
 }
