00ed39f6b4234bbd80256fb2ea2f365f67bc1fc1
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-10-01 18:16:16 +0200

    Update Carrito.tsx \ Created Delete Product on order Logic

--- src/pages/Carrito.tsx
+++ src/pages/Carrito.tsx
@@ -3,19 +3,23 @@ import Layout from "../layout/Layout";
 import { carritoAtom, userAtom } from "../store/store";
 import { useEffect, useState } from "react";
 import { isTokenValid } from "../lib/Auth";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 import {
+    Carrito,
   NormalizedCarrito,
   getTotal,
   normalizeCarrito,
 } from "../types/carrito";
 import CarritoComponent from "../components/CarritoComponent";
+import { toast, ToastContainer } from "react-toastify";
+import { saveOrder } from "../lib/Api";
+import { Product } from "../types/product";
 
-export default function Carrito() {
+export default function CarritoPage() {
   const [user] = useAtom(userAtom);
-  const [carrito] = useAtom(carritoAtom);
-  const [normalizedCarrito, setNormalizedCarrito] = useState<NormalizedCarrito>(
-    []
-  );
+  const [carrito, setCarrito] = useAtom(carritoAtom);
+  const [normalizedCarrito, setNormalizedCarrito] = useState<NormalizedCarrito>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   // Check if JWT token is Valid, if not, redirect to login.
   // First atom load will always set user to null, this is why I have to check the user this way..
@@ -32,8 +36,43 @@ export default function Carrito() {
     setNormalizedCarrito(normalizeCarrito(carrito));
   }, []);
 
+  const clearCarrito = () => {
+    setCarrito([]);
+    setNormalizedCarrito([]);
+  };
+
+  const makeOrder = async () => {
+    setLoading(true);
+    const orderResponse = await saveOrder(normalizedCarrito);
+    if (orderResponse === 201) {
+      toast.success("¡Gracias por tu compra!");
+      clearCarrito();
+    } else {
+      toast.error(
+        "Ups!, algo ha ido mal, quizá haya intentado comprar mas cantidad de la que hay disponible de algún producto. Comprueba su compra y si no funciona, prueba de nuevo más tarde."
+      );
+    }
+    setLoading(false);
+  };
+
+  const deleteProduct = (producto: Product) => {
+    if (producto.quantity - 1 <= 0) {
+      const newCarrito = carrito.filter((p) => p.id !== producto.id);
+      setCarrito(newCarrito);
+      setNormalizedCarrito(normalizeCarrito(newCarrito));
+    } else {
+        // Delete one quantity of the product
+        const index = carrito.findIndex((p) => p.id === producto.id);
+        const newCarrito = [...carrito];
+        newCarrito.splice(index, 1);
+        setCarrito(newCarrito);
+        setNormalizedCarrito(normalizeCarrito(newCarrito));
+    }
+  };
+
   return (
     <Layout user={user}>
+      <ToastContainer />
       <button className="btn btn-ghost" onClick={() => window.history.back()}>
         Volver atrás
       </button>
@@ -48,17 +87,41 @@ export default function Carrito() {
             {normalizedCarrito.map((product) => (
               <CarritoComponent
                 producto={product}
-                quantity={product.quantity}
+                handleDelete={deleteProduct}
               />
             ))}
           </div>
         </div>
         <div className="w-1/4 border-blue-700 border-2 p-6 rounded-xl">
+          <span
+            className="float-right text-xl hover:cursor-pointer active:scale-90 transition-all"
+            title='La "compra" ha realizar no es real, no se cobrará un importe. Es solo un ejemplo'
+            onClick={() =>
+              toast.info(
+                'La "compra" ha realizar no es real, no se cobrará un importe. Es solo un ejemplo'
+              )
+            }
+          >
+            <BsFillInfoCircleFill />
+          </span>
           <h1 className="text-2xl">Total: {getTotal(normalizedCarrito)}€</h1>
           <h2>Total de artículos: {normalizedCarrito.length}</h2>
-          <button className="btn btn-block btn-primary mt-4">
-            ¡Comprar ahora!
+          <button
+            className="btn btn-block btn-primary mt-4"
+            onClick={makeOrder}
+            disabled={normalizedCarrito.length === 0}
+          >
+            {!loading && "¡Comprar ahora!"}
+            {loading && <span className="loading loading-dots"></span>}
           </button>
+          {normalizedCarrito.length !== 0 && (
+            <p
+              className="text-center mt-2 text-sm text-red-300 hover:underline cursor-pointer"
+              onClick={clearCarrito}
+            >
+              Vaciar el carrito
+            </p>
+          )}
         </div>
       </div>
     </Layout>
